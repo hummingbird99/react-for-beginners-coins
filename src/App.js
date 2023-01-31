bcd15c469d0078e832f9aaeed89a879e83e7839f
hummingbird99 <sseoulite@gmail.com>
2023-02-01 02:09:07 +0900

    Create coin converter

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const deleteBtn = (index) => {
-    setToDos((curToDos) =>
-      curToDos.filter((_, curIndex) => curIndex !== index)
-    );
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [choosenCoin, setChoosenCoin] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (e) => {
+    setMoney(() => e.target.value);
   };
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // ...: 배열의 원소 나열
-    setToDo("");
+  const onSelect = (e) => {
+    setChoosenCoin(e.target.value);
   };
-  console.log(toDos);
+
   return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteBtn(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>
+        Top-ranked Coins! {loading ? "" : `<${coins.length}>`}
+      </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <div className={styles.box}>
+            <label className={styles.label}>Amount you have: $</label>
+            <input
+              onChange={onChange}
+              value={money}
+              className={styles.input}
+              type="number"
+            ></input>
+            <br />
+            <label className={styles.label}>Choose coin: </label>
+            <select onChange={onSelect} className={styles.select}>
+              <option style={{ textAlign: "center" }}>
+                ----- Choose options ------
+              </option>
+              {coins.map((coin) => (
+                <option
+                  key={coin.id}
+                  value={[coin.name, coin.quotes.USD.price]}
+                >
+                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+                </option>
+              ))}
+            </select>
+          </div>
+          <hr className={styles.hr} />
+          <div className={styles.result}>
+            {choosenCoin !== "" ? (
+              <strong>
+                You can get {Math.floor(money / choosenCoin.split(",")[1])}{" "}
+                {choosenCoin.split(",")[0]}(s)!
+              </strong>
+            ) : (
+              <strong>Please fill out the amount.</strong>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
